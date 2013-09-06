1cda3b80a9a276d602dca90fa39eab3cfda64119
Marcin Matlaszek <marcin.matlaszek@infakt.pl>
2013-09-06 09:14:12 +0200

    Setup.py fixed

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ except ImportError:
 
 config = {
     'description': 'WordPress Updater',
-    'author': 'Marcin Matlaszek, inFakt DevTeam',
-    'url': 'github.com/infakt/wpupdater',
-    'download_url': 'github.com/infakt/wpupdater',
-    'author_email': 'marcin.matlaszek@infakt.pl, devteam@infakt.pl',
-    'version': '0.1',
+    'author': 'Marcin Matlaszek <emate>, inFakt DevTeam',
+    'url': 'http://github.com/infakt/wpupdater',
+    'download_url': 'http://github.com/infakt/wpupdater',
+    'author_email': 'mmatlaszek@gmail.com, marcin.matlaszek@infakt.pl, devteam@infakt.pl',
+    'version': '0.1-f1',
     'install_requires': ['lxml'],
     'packages': ['wpupdater'],
     'scripts': ['bin/wpupdate.py'],
